da7dbdf311ab2f8fd1a4032dcb23f2decf9d28b3
Saumya522 <92753765+Saumya522@users.noreply.github.com>
2021-10-19 00:41:08 +0530

    Servo Motor

---
+++ servo_motor.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+Servo servo;
+int angle = 10;
+void setup() {
+  servo.attach(8);
+  servo.write(angle);
+}
+void loop() 
+{ 
+ // scan from 0 to 180 degrees
+  for(angle = 0; angle < 180; angle++)  
+  {                                  
+    servo.write(angle);               
+    delay(15);                   
+  } 
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 0; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(15);       
+  } 
+}
